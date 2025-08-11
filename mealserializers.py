8a6610d473a11144ec9b544e9d9a326926c432ef
sanasyed521 <senooaltaf@gmail.com>
2025-08-11 16:21:53 -0400

    Delete mealserializers.py

--- mealserializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Meal
-
-class MealSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Meal
-        fields = ['id', 'name', 'description', 'calories', 'protein', 'carbs', 'fat', 'date', 'time']
-        read_only_fields = ['id']
