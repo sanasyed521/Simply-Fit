c5217488490ede246092a8c8ae90c196e6e2bc0f
sanasyed521 <senooaltaf@gmail.com>
2025-08-11 16:22:57 -0400

    Delete workoutserializer.py

--- workoutserializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Workout
-
-class WorkoutSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Workout
-        fields = ['id', 'workout_type', 'duration', 'intensity', 'calories_burned', 'date', 'notes']
-        read_only_fields = ['id']
